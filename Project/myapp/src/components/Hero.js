72f5297efc965530cdf8c3c4f6e536f538e4d7a1
Anushka-Gamad <anushkagamad553@gmail.com>
2022-12-09 17:38:29 +0530

    backend

--- Project/myapp/src/components/Hero.js
+++ Project/myapp/src/components/Hero.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Axios from 'axios';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 //importing hero data
 import {heroData} from '../data';
@@ -6,7 +9,33 @@ import {heroData} from '../data';
 //importing components
 import Header from '../components/Header';
 
-const Hero = () => {
+const Hero = (props) => {
+
+  //creating state variable
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const register = () => {
+    Axios.post('http://localhost:3001/register', 
+    {
+      username : name,
+      useremail: email
+    }).then((response) => {
+      console.log(response)
+    });
+  };
+
+  // const dispatch = useDispatch();
+
+  // //function onCreateAcc
+  // function onCreateAcc(e) {
+  //   e.preventDefault();
+  //   const postData = {
+  //     name,
+  //     email,
+  //   };
+  // }
+
   //destructoring hero data
   const {title,subtitle,btnText,image} = heroData;
 
@@ -22,18 +51,21 @@ const Hero = () => {
               <h1 className='text-[60px] h1 xl:max-w-[1100px] mb-6 lg:mb-12' data-aos='fade-down' data-aos-delay='400'>{title}</h1>
               <p className='lead xl:max-w-[470px] mb-6 lg:mb-12' data-aos='fade-down' data-aos-delay='500'>{subtitle}</p>
                <div className="md:p-5 md:mx-5">
-                    <form>
+                    <form  >
                         <div className='mb-4'>
-                          <input className="bg-primary shadow appearance-none border rounded-full w-48 py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline rounded-[50px]" id="username" type="text" placeholder="Enter Your Name" data-aos='fade-down' data-aos-delay='600'/>
+                          <input className="bg-primary shadow appearance-none border rounded-full w-48 py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline rounded-[50px]" id="username" type="text" placeholder="Enter Your Name" data-aos='fade-down' data-aos-delay='600'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
                         </div>
                         <div className='mb-4'>
-                        <input className="bg-primary shadow appearance-none border rounded-full w-48 py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline rounded-[50px]" id="username" type="text" placeholder="Enter Your Email" data-aos='fade-down' data-aos-delay='700'/>
+                        <input className="bg-primary shadow appearance-none border rounded-full w-48 py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline rounded-[50px]" id="username" type="text" placeholder="Enter Your Email" data-aos='fade-down' data-aos-delay='700'
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
                         </div>
-                        {/* <div className='mb-4'>
-                          <button className='btn btn-primary mb-8 xl:mb-0' type="button">{btnText}</button>
-                        </div> */}
                         <div className='mb-4'>
-                        <button className="inline-block w-48 px-5 py-3 text-secondary font-semibold  rounded-[50px] shadow-md hover:bg-purple-700 hover:shadow-lg hover:text-white  mb-8 xl:mb-0 border border-slate-300 hover:border-indigo-300"
+                        <button onClick={register} className="inline-block w-48 px-5 py-3 text-secondary font-semibold  rounded-[50px] shadow-md hover:bg-purple-700 hover:shadow-lg hover:text-white  mb-8 xl:mb-0 border border-slate-300 hover:border-indigo-300"
                                 type="button" data-aos='fade-down' data-aos-delay='800'>{btnText}</button>
                         </div>
                       </form>
---
+++ Project/server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors"); 
+
+const app = express();
+
+//this I'm using to parse every object sent from frontend to backend
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    user : "root",
+    host:"localhost",
+    password: "Anu1234#",
+    database:"userdb"
+});
+
+//backend route
+app.post('/register',(req, res) => {
+
+    const name  = req.body.username
+    const email = req.body.useremail
+
+
+    db.query("INSERT INTO user (name, email) VALUES(?,?) ",
+    [name, email],
+    (err,result) =>{
+        console.log(err);
+    });
+});
+
+app.listen(3001,()=>{
+    console.log("running server");
+})
